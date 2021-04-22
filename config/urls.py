b1efbe236a6e2e03689f409d78f62928c14bc429
Rustam Kamalov <74410332+rikamalov@users.noreply.github.com>
2021-04-22 03:29:04 +0300

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -18,7 +18,7 @@ from django.urls import path
 from crm import views
 from django.conf.urls.static import static
 from django.conf import settings
-from settings import DEBUG
+from .settings import DEBUG
 
 urlpatterns = [
     path('admin/', admin.site.urls),
