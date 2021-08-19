3b0929ac6a9d99f54182f77d1328c7fec55dfd89
Rustam Kamalov <74410332+rikamalov@users.noreply.github.com>
2021-08-19 15:57:35 +0300

    Update models.py

--- crm/models.py
+++ crm/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class StatusCrm(models.Model):
-    Status_name = models.CharField(max_length=200, verbose_name='Название статуса')
+    status_name = models.CharField(max_length=200, verbose_name='Название статуса')
 
     def __str__(self):
         return self.Status_name
