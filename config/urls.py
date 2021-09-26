e761e56d6f9e6f51995ef71c6927e46da3b66bfa
Rustam Kamalov <74410332+rikamalov@users.noreply.github.com>
2021-09-26 06:18:12 +0300

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -18,12 +18,12 @@ from django.urls import path
 from crm import views
 from django.conf.urls.static import static
 from django.conf import settings
-from .settings import DEBUG
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.first_page),
     path('thanks/', views.thanks_page, name='thanks_page')
 ]
-if DEBUG:
+if settings.DEBUG:
     urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
